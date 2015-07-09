62854bc11de9a52119fddd247d66d49965214643
Freedom <eoaion@gmail.com>
2015-07-09 15:40:08 +0800

    第一版框架搭建

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,56 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 3
+
+// FMDB
+#define COCOAPODS_POD_AVAILABLE_FMDB
+#define COCOAPODS_VERSION_MAJOR_FMDB 2
+#define COCOAPODS_VERSION_MINOR_FMDB 5
+#define COCOAPODS_VERSION_PATCH_FMDB 0
+
+// FMDB/common
+#define COCOAPODS_POD_AVAILABLE_FMDB_common
+#define COCOAPODS_VERSION_MAJOR_FMDB_common 2
+#define COCOAPODS_VERSION_MINOR_FMDB_common 5
+#define COCOAPODS_VERSION_PATCH_FMDB_common 0
+
+// FMDB/standard
+#define COCOAPODS_POD_AVAILABLE_FMDB_standard
+#define COCOAPODS_VERSION_MAJOR_FMDB_standard 2
+#define COCOAPODS_VERSION_MINOR_FMDB_standard 5
+#define COCOAPODS_VERSION_PATCH_FMDB_standard 0
+
+// SDCycleScrollView
+#define COCOAPODS_POD_AVAILABLE_SDCycleScrollView
+#define COCOAPODS_VERSION_MAJOR_SDCycleScrollView 1
+#define COCOAPODS_VERSION_MINOR_SDCycleScrollView 3
+#define COCOAPODS_VERSION_PATCH_SDCycleScrollView 0
+
+// Starscream
+#define COCOAPODS_POD_AVAILABLE_Starscream
+#define COCOAPODS_VERSION_MAJOR_Starscream 0
+#define COCOAPODS_VERSION_MINOR_Starscream 9
+#define COCOAPODS_VERSION_PATCH_Starscream 3
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 0
+
+// YouTubePlayer
+#define COCOAPODS_POD_AVAILABLE_YouTubePlayer
+#define COCOAPODS_VERSION_MAJOR_YouTubePlayer 0
+#define COCOAPODS_VERSION_MINOR_YouTubePlayer 2
+#define COCOAPODS_VERSION_PATCH_YouTubePlayer 0
+
